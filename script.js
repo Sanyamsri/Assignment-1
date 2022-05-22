e821294402b01bd512d4d97a53e2e29f6a7a34f2
sanyamsri <sanyam.srivastava@sprinklr.com>
2022-05-22 23:05:04 +0530

    third commit - added selected index feature and abilty to update th eselected index and refactored code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import imageData from "./imageData.js";
 
+let selectedIndex = 0;
+
 function fillListItem(listItem, { previewImage, title }, index) {
   const newTitle = shortenTitle(title);
   listItem.setAttribute("id", `item-${index}`);
@@ -15,7 +17,26 @@ function fillListItem(listItem, { previewImage, title }, index) {
     listItem = fillListItem(listItem, element, index);
     ul.appendChild(listItem);
   });
+  showSelected();
 })();
+
+function updateSelectedIndex(newIndex) {
+  newIndex = validateIndex(newIndex);
+  const selectedItem = document.querySelector(`#item-${selectedIndex}`);
+  selectedItem.classList.remove("selected");
+  selectedIndex = newIndex;
+  showSelected();
+}
+
+function showSelected() {
+  const picture = document.querySelector(".picture");
+  const caption = document.querySelector("textarea");
+  const selectedItem = document.querySelector(`#item-${selectedIndex}`);
+  selectedItem.classList.add("selected");
+  const selectedImage = imageData[selectedIndex];
+  picture.src = selectedImage.previewImage;
+  caption.value = selectedImage.title;
+}
 
 function shortenTitle(title) {
   if (title.length < 20) return title;
